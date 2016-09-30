9f0e81cb7d34ac749574024393c8afd893472aee
adamhleslie <adamhleslie@gmail.com>
2016-09-30 09:38:21 -0500

    Adds component implementation

---
+++ Source/Core/Component.cpp
@@ -0,0 +1,49 @@
+#include "Component.h"
+
+#include <cassert>
+
+#include "Entity.h"
+
+void Component::onLoad (Entity* entity)
+{
+	assert(!mLoaded);
+	assert(entity);
+	
+	mEntity = entity;
+	mLoaded = true;
+
+	onLoadCallback();
+}
+
+void Component::onUnload (Entity* entity)
+{
+	assert(mLoaded);
+	assert(mEntity == entity);
+	
+	mEntity = nullptr;
+	mLoaded = false;
+}
+
+bool Component::isLoaded ()
+{
+	return mLoaded;
+}
+
+bool Component::isEnabled ()
+{
+	return mEnabled;
+}
+
+void Component::enable ()
+{
+	mEnabled = true;
+	if (mLoaded)
+		mEntity->enableComponent(this);
+}
+
+void Component::disable ()
+{
+	mEnabled = false;
+	if (mLoaded)
+		mEntity->enableComponent(this);
+}
