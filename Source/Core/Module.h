61354c5935bbe598b7f5b69885f41d6b3e776f58
adamhleslie <adamhleslie@gmail.com>
2016-10-02 00:19:08 -0500

    Updates Module creation instructions

--- Source/Core/Module.h
+++ Source/Core/Module.h
@@ -4,8 +4,7 @@
 	To create a brand new module:
 		0. Create a new folder for it, and add it to CMakeLists.txt with add_project_module
 		1. Inherit from this class
-		2. Implement constructor and destructor as needed
-			a. Call disable() within your constructor if you don't want your module to update upon load
+		2. Call disable() within your constructor (or onLoadCallback) if you don't want your module to update upon load
 		3. Increase Core::kNumModules (Core.h)
 		4. Add your module to Core::loadModules() (Core.cpp)
 		5. Add functionality as needed - Put related components into the modules folder
