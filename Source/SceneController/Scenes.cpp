cf6bab031460b2dae15db6f01ae8d1ec338ee087
adamhleslie <adamhleslie@gmail.com>
2016-10-31 20:09:32 -0500

    Fixes force application

--- Source/SceneController/Scenes.cpp
+++ Source/SceneController/Scenes.cpp
@@ -65,9 +65,12 @@ namespace wellGame
 		constexpr float kScale = .1;
 		Entity* sphere = core->createEntity(kSphere, "Chrome", true, Vector3(kScale, kScale, kScale), Vector3(0, 200, 0));
 		if (createRigidbody)
+		{
 			sphere->getTransform()->attachRigidbody(kSphere, Vector3(kScale * 50, 0, 0), 1, 1, true);
-		btVector3 tmp1(0,-100000/2,0);
-		sphere->getTransform()->mRigidBody->applyCentralForce(tmp1);
+			btVector3 tmp1(0,-100000/2,0);
+			sphere->getTransform()->mRigidBody->applyCentralForce(tmp1);
+		}
+		
 		sphere->isBall = true;
 		AudioPlayer* ap = sphere->createComponent<AudioPlayer>();
 
@@ -158,7 +161,7 @@ namespace wellGame
 void SceneController::initScenes ()
 {
 	// Scene scene1(wellGame::loadSingleplayer);
-	Scene scene1(wellGame::loadGameServer);
-	// Scene scene1(wellGame::loadGameClient);
+	// Scene scene1(wellGame::loadGameServer);
+	Scene scene1(wellGame::loadGameClient);
 	addScene(scene1);
 }
